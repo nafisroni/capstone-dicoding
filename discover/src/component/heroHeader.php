cce0314142f46746eb041cee428a5dac03b808f2
GilangAddh <96603125+GilangAddh@users.noreply.github.com>
2022-12-11 11:36:09 +0700

    upload folder komponen untuk
    upload komponen head, hero header dan hero header

---
+++ discover/src/component/heroHeader.php
@@ -0,0 +1,31 @@
+<div class="hero">
+        <nav class="navbar navbar-expand-lg bg-transparent pt-2">
+            <div class="container-fluid">
+              <a class="navbar-brand fw-bold" href="#">WILO APPS</a>
+              <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon "></span>
+              </button>
+              <div class="collapse navbar-collapse offset-7" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Explore</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="btn btn-primary nav-link text-white p-1">LOGIN</a>                    
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+
+        <div class="hero-text row">
+                <h1 class="hero-title">Temukan Wisatamu</h1>
+                  
+                <p class="hero-subtitle">
+                  Ribuan tempat wisata sedang menanti kehadiran anda
+                </p>
+        </div>
+    </div>
